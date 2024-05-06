bb746c1d770c9f6f6f142b68ded90a0d15637c6f
Sebastian Abello <juanse.abello.2004@gmail.com>
2024-05-05 21:48:10 -0500

    first commit

---
+++ src/main/java/com/gooplanycol/gooplany/GooplanyApplication.java
@@ -0,0 +1,13 @@
+package com.gooplanycol.gooplany;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GooplanyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GooplanyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gooplanycol/gooplany/GooplanyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gooplanycol.gooplany;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GooplanyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
